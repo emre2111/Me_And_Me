f60cb78c9926fa05099e2391869e212ad098301d
Maximilian Gaethje <maximilian.gaethje@outlook.com>
2019-02-27 11:40:05 +0100

    Search implemented

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,13 +1,14 @@
 class PlacesController < ApplicationController
   def index
-    @places = if params[:continent]
-      Place.where[:continent]
-    else
-      Place.all
-    end
+    # @places = Place.find(params[place_params])
+    @places = Place.where(continent: params[:continent], level_of_remoteness: params[:level_of_remoteness])
+    @places
   end
 
   def show
     @place = Place.find(params[:id])
   end
+
 end
+
+# http://localhost:3000/places?utf8=%E2%9C%93&continent=Africa&level_of_remoteness=extremely+remote&commit=Search
