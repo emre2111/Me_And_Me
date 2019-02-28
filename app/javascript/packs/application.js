3ba3e920e07711a4c9a1a2175d3b673855efbf8d
Emre Yilmaz <emre211@hotmail.com>
2019-02-28 12:41:44 +0100

    DatePicker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,11 @@
 import "bootstrap";
+import flatpickr from "flatpickr"
+import "flatpickr/dist/themes/airbnb.css" // Note this is important!
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
+
+
+flatpickr("#start_date", {
+  altInput: true,
+  plugins: [new rangePlugin({ input: "#end_date"})]
+
+});
